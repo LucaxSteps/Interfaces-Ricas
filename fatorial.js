f7df45046022bed3b929378a79677206a93436d3
LucaxSteps <161722520+LucaxSteps@users.noreply.github.com>
2024-11-08 18:19:50 +0000

    :sparkles: feat: testes de fatorial e soma feitos

---
+++ fatorial.js
@@ -0,0 +1,13 @@
+function factorial(n){
+    var result = n;
+     if (n === 0 || n===1){
+         return 1;
+     }
+     while (n > 1){
+         n--;
+         result*=n;
+     }
+     return result;
+ }
+ module.exports = factorial;
+
